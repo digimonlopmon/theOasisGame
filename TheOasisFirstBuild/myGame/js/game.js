84c09bebfcea0b237d2cb2b5dd70924e1405f967
Wenbo Xie <wxie10@ucsc.edu>
2018-05-13 16:03:49 -0700

    TheOasisFirstBuild
    Changes:
    1. bgm bug fixed
    2. player cannot kill while obtaining water

---
+++ TheOasisFirstBuild/myGame/js/game.js
@@ -0,0 +1,12 @@
+//create the game
+var game = new Phaser.Game(600, 600, Phaser.AUTO, 'gameDiv');
+
+//add all states
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('menu', menuState);
+game.state.add('play', playState);
+game.state.add('over', overState);
+
+//start from the boot state
+game.state.start('boot');
