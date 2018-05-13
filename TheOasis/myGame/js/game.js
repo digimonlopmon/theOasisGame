b7df38a18bd3ea344d1350bd95c0f2b38df5b67a
Wenbo Xie <wxie10@ucsc.edu>
2018-05-13 11:01:04 -0700

    changes added
    added gameover state, random spawn enemies. enemies move to oasis automatically, energy drink spawn each 15 sec at the top right corner, press 'X' to eat enemies when player is overlapping with enemies and press 'C' to obtain energy drink when overlapping with it.

--- TheOasis/myGame/js/game.js
+++ TheOasis/myGame/js/game.js
@@ -6,6 +6,7 @@ game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
 game.state.add('play', playState);
+game.state.add('over', overState);
 
 //start from the boot state
 game.state.start('boot');
--- TheOasis/myGame/js/over.js
+++ TheOasis/myGame/js/over.js
@@ -1,4 +1,4 @@
-var menuState = {
+var overState = {
 	create: function(){
 
 		//create title screen background
@@ -6,20 +6,20 @@ var menuState = {
         //title screen bgm looping
         
         //title screen text
-		var nameLabel = game.add.text(170, 150, 'The Oasis', {font: '60px', fill: '#ffffff'});
+		var overLabel = game.add.text(170, 150, 'Gameover!', {font: '60px', fill: '#ffffff'});
 
-		var startLabel = game.add.text(210, game.world.height-100, 'Press SPACE to Start', {font: '20px', fill: '#fff'});
+		var restartLabel = game.add.text(210, game.world.height-100, 'Press SPACE back to Menu', {font: '20px', fill: '#fff'});
 
         //add space input for starting the game
 		var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
         //press space to start the game
-		space.onDown.addOnce(this.start, this);
+		space.onDown.addOnce(this.restart, this);
 	},
 
-    start: function(){
+    restart: function(){
     	//state changing
-    	game.state.start('play');
+    	game.state.start('menu');
 
     	//stop looping the title screen bgm
 
