797a40d76b3df8efe6611f92085d20429ccf7166
Mohan Jia <mojia@ucsc.edu>
2018-06-05 11:50:48 -0700

    adding a BLOOD EFFECT
    blood effect!!!

---
+++ TheOasisVer7/myGame/js/win1.js
@@ -0,0 +1,40 @@
+var bgm;
+var win;
+var win1State = {
+	create: function(){
+
+		//create background
+        win = game.add.image(0, 0, 'success');
+      
+        //bgm
+        bgm = game.add.audio('win');
+        bgm.loop = false;
+        bgm.play();
+        
+        //text
+		var nameLabel = game.add.text(300, 150, 'You successfully protected the Oasis!', {font: '32px', fill: '#04A713'});
+        nameLabel.anchor.setTo(0.5, 0.5);
+
+        //back to title screen
+        back_label = game.add.text(120, 50, 'Back to Title Screen', {font: '24px', fill: '#3399FF'});
+        back_label.stroke = '#000000';
+        back_label.strokeThickness = 8;
+        back_label.anchor.setTo(0.5, 0.5);
+        back_label.inputEnabled = true;
+        back_label.events.onInputUp.add(function () {
+            game.state.start('menu');
+            bgm.pause();
+        });
+
+        //next level
+        next_label = game.add.text(500, 50, 'Start next level', {font: '24px', fill: '#3399FF'});
+        next_label.stroke = '#000000';
+        next_label.strokeThickness = 8;
+        next_label.anchor.setTo(0.5, 0.5);
+        next_label.inputEnabled = true;
+        next_label.events.onInputUp.add(function () {
+            game.state.start('medium');
+            bgm.pause();
+        });
+	},
+};
